76dfd6244e117fe69815a948a62fb875f1ddd568
SANTHOSH A <arussanthosh@gmail.com>
2025-07-08 22:37:45 +0530

    Add

---
+++ TUTO/TU1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class TU1{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter The Number");
+        int a =in.nextInt();
+        System.out.println(a);
+    }
+}
