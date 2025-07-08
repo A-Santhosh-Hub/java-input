fb7e4f37fae0ddebaf4aa2ad2669da938b136e3d
SANTHOSH A <arussanthosh@gmail.com>
2025-07-08 22:50:16 +0530

    Add

---
+++ TUTO/Name.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Name{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Your Good Name Sir :");
+         String name =in.nextLine();
+         System.out.println("Hey Hai Mr: "+name+". Well Come to the Code Hub... ");
+
+
+    }
+}
