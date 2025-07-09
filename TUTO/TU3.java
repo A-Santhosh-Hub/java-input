7f4477d8c484150b79d84dd3a21806b3d78352bb
SANTHOSH A <arussanthosh@gmail.com>
2025-07-09 22:29:34 +0530

    Update TU3.java

--- TUTO/TU3.java
+++ TUTO/TU3.java
@@ -5,13 +5,13 @@ class TU3 {
     System.out.println("Enter the Two Number's"); 
         int num1=in.nextInt();
         int num2=in.nextInt();
-        if(num1>num2){
-        System.out.println("The Number: "+num1+" is Big "+(num1>num2));
+        if(num1==num2){
+        System.out.println("The Number: "+num1+" is Equal ");
         }
 
         else{
 
-        System.out.println("The Number: "+num1+" is Summall "+(num1>num2));
+        System.out.println("The Number: "+num1+" is Not Equal ");
         }
     }
 }
