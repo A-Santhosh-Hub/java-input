bd80f93b15ee35a0af2e289b3637806874f6a012
SANTHOSH A <arussanthosh@gmail.com>
2025-07-09 22:04:12 +0530

    Add TU2

---
+++ TUTO/TU2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class TU2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);    
+        System.out.println("Enter Your Name:");
+        String name =in.nextLine();
+        System.out.println("Enter Your Age:");
+        int a = in.nextInt();  
+        System.out.println("Hai Mr."+name);
+        System.out.println("Mr."+name+" Your Age is "+a);
+        
+
+
+    }
+}
