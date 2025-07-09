582299462346ab0e30a1cb8e9d6002f62b582569
SANTHOSH A <arussanthosh@gmail.com>
2025-07-09 22:34:36 +0530

    Create TU4.java

---
+++ TUTO/TU4.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class TU4 {
+   public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String a="apple";
+    // String b="apple";
+
+
+
+    System.out.println(a.equals("apple"));
+   } 
+}
