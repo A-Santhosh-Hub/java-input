452cec1adb77a45ea5310408489170900dcb481c
SANTHOSH A <arussanthosh@gmail.com>
2025-07-09 22:14:13 +0530

    ADD

---
+++ TUTO/TU3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class TU3 {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);    
+        // System.out.println("Enter The Number:");
+        // int  a = in.nextInt();
+        // System.out.println("Enter The Number:");
+        // int b= in.nextInt();
+        // int c=a+b;
+        // System.out.println(c);
+        System.out.println("Enter Your Name:");
+        String name=in.nextLine();
+        System.out.println(name.charAt(2));
+
+    }
+}
