d592098832705821d4e539af53d3326ade823d0b
SANTHOSH A <arussanthosh@gmail.com>
2025-07-10 21:05:08 +0530

    Add

---
+++ TUTO/TU5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class TU5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        int a  = in.nextInt();
+
+        if(a<7000){
+            System.out.println("Your fail");
+        }
+        else{
+            System.out.println("you Pass");
+        }
+    }
+}
