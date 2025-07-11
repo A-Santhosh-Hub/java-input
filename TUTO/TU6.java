6f6867d9a4d756804b15d7a730e1879a38a394b3
SANTHOSH A <arussanthosh@gmail.com>
2025-07-11 21:12:33 +0530

    Add

---
+++ TUTO/TU6.java
@@ -0,0 +1,9 @@
+public class TU6 {
+    public static void main(String[] args) {
+        String [] name= new String[3];
+        name [1]="SANTHOSH";
+        name[0]="A";
+        name[2]="B";
+        System.out.println(name[1]);
+    }
+}
