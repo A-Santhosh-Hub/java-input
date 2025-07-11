c3d7f020eb82d815d2e77700e6f19bbce5472e0a
SANTHOSH A <arussanthosh@gmail.com>
2025-07-11 21:16:08 +0530

    Update TU6.java

--- TUTO/TU6.java
+++ TUTO/TU6.java
@@ -1,9 +1,11 @@
 public class TU6 {
     public static void main(String[] args) {
+        String [] ABC= {"A","B","C","D"};
         String [] name= new String[3];
         name [1]="SANTHOSH";
         name[0]="A";
         name[2]="B";
         System.out.println(name[1]);
+        System.out.println(ABC[1]);
     }
 }
